2fa586da4d2bfd02fcb8879fec6f7749b9369cd2
woyaoyulang <z1679619459@qq.com>
2019-09-25 20:08:50 +0800

    fastjson转换器

--- src/main/java/com/clare/config/WebMvcConfig.java
+++ src/main/java/com/clare/config/WebMvcConfig.java
@@ -1,18 +1,27 @@
 package com.clare.config;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.clare.interceptor.LoginInterceptor;
 import com.clare.interceptor.SystemInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import javax.print.attribute.standard.Media;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * springmvc配置
@@ -67,6 +76,35 @@ public class WebMvcConfig implements WebMvcConfigurer {
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new CorsFilter(source));
         return filterRegistrationBean;
     }
+
+    /**
+     * 自定义字符串转化
+     * @return
+     */
+    @Bean
+    public HttpMessageConverter<String > stringHttpMessageConverter(){
+        StringHttpMessageConverter messageConverter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
+        return messageConverter;
+    }
+
+
+    @Bean
+    public HttpMessageConverter fastJsonHttpMessageConverters(){
+        //1、需要定义一个converter转换信息的对象
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        //2、添加fastjson的配置信息
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        //3、处理中文乱码问题
+        List<MediaType> mediaTypes = new ArrayList<>();
+        mediaTypes.add(MediaType.APPLICATION_JSON_UTF8);
+        //4、在convert中添加配置信息
+        fastConverter.setSupportedMediaTypes(mediaTypes);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        return fastConverter;
+    }
+
+
 
 
 }
