d9abc53566d472f7be432c77c7f4b3dde7dca152
zhanghao <z1679619459@163.com>
2020-12-17 18:28:48 +0800

    格式修改

--- src/main/java/com/rowan/mapper/UserMapper.java
+++ src/main/java/com/rowan/mapper/UserMapper.java
@@ -8,6 +8,5 @@ import org.springframework.stereotype.Repository;
  * @author : zhangHao
  * @data : 2019-07-31 19:28
  */
-@Repository
 public interface UserMapper extends MybatisBaseMapper<User> {
 }
