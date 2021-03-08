4b326652f164eb6537e5e64be960126793ca46c0
zhanghao <z1679619459@163.com>
2021-03-08 20:52:01 +0800

    测试类修改

---
+++ src/test/java/com/rowan/clazz/Cleanser.java
@@ -0,0 +1,10 @@
+package com.rowan.clazz;
+
+public class Cleanser {
+
+    private String s = new String("Cleanser");
+
+    public static void main(String[] args) {
+        System.out.println("this is cleanser");
+    }
+}
---
+++ src/test/java/com/rowan/clazz/Detergent.java
@@ -0,0 +1,10 @@
+package com.rowan.clazz;
+
+public class Detergent extends Cleanser {
+
+    public static void main(String[] args) {
+        System.out.println(args.length);
+        System.out.println("this is detergent");
+        Cleanser.main(args);
+    }
+}
